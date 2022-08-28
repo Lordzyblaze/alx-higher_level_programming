d964643610f0b02b4553eac5b54793378e3f3aa9
Adams Adjartey <melawzy421@yahoo.com>
2022-08-28 10:38:39 +0000

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for c in my_string:
-       if c not in "Cc":
-            new_string += c
-    return new_string
+    listofchars = list(my_string)
+    for lens in listofchars:
+        if lens == 'c' or lens == 'C':
+            listofchars.remove(lens)
+    return("".join(listofchars))
