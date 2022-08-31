b8c9d3ffacb587b5578269aee2da3ac3973c475f
Adams Adjartey <melawzy421@yahoo.com>
2022-08-31 23:12:04 +0000

    11. Multiply by using map

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if my_list:
-        return sum(x * y for x, y in my_list) / sum(y for x, y in my_list)
-    return 0
-    # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda i: i * number, my_list))
