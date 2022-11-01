2d4e8bbec4790ab43ddd50748e9d5db4e8f7d47e
Adams Adjartey <melawzy421@yahoo.com>
2022-11-01 21:24:29 +0000

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,7 +1,2 @@
--- create table called first_table in the current database in your MySQL server.
--- The database name will be passed as an argument of the mysql command
--- If the table first_table already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS first_table (
-        id INT,
-        NAME VARCHAR(256)
-);
+-- getting the average of all scores in second_table
+SELECT AVG(score) AS average FROM second_table;
